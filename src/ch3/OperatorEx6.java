c2e15f9b62668254379f26aba1a950ccefd777ea
Siru <ehdgur9553@naver.com>
2022-03-13 10:52:42 +0900

    3/13 commit

---
+++ src/ch3/OperatorEx6.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx6 {
+	public static void main(String[] args) {
+		byte a = 10;
+		byte b = 20;
+		byte c = a + b; //컴파일  에러가 발생, 따라서 명시적 형변환이 필요 byte c = (byte)(a+b);
+		System.out.println(c);
+	}
+}
