7ac31797863aa3d6fd158099270bc96826170029
Siru <ehdgur9553@naver.com>
2022-03-14 19:55:47 +0900

    3/14 commit (solve className error)

--- src/ch3/OperatorEx24.java
+++ src/ch3/OperatorEx24.java
@@ -2,15 +2,26 @@ package ch3;
 
 public class OperatorEx24 {
 	public static void main(String[] args) {
-		String str1 = "abc";
-		String str2 = new String("abc");
+		int x = 0;
+		char ch = ' ';
+
+		x = 15;
+		System.out.printf("x=%2d, 10 < x && x < 20 = %b\n", x, 10 < x && x < 20);
+
+		x = 6;
+		System.out.printf("x=%2d, x%%2==0 || x%%3==0 && x%%6!=0 = %b\n", x, x % 2 == 0 || x % 3 == 0 && x % 6 != 0);
+		System.out.printf("x=%2d, (x%%2==0 || x%%3==0) && x%%6!=0 = %b\n", x, (x % 2 == 0 || x % 3 == 0) && x % 6 != 0);
 		
-		System.out.printf("\"abc\"==\"abc\" ? %b\n", "abc"=="abc");
-		System.out.printf(" str1==\"abc\" ? %b\n", str1=="abc");
-		System.out.printf(" str2==\"abc\" ? %b\n", str2=="abc");
-		System.out.printf("str1.equals(\"abc\") ? %b\n", str1.equals("abc"));
-		System.out.printf("str2.equals(\"abc\") ? %b\n", str1.equals("abc"));
-		System.out.printf("str2.equals(\"ABC\") ? %b\n", str2.equals("ABC"));
-		System.out.printf("str2.equalsIgnoreCase(\"ABC\") ? %b\n", str2.equalsIgnoreCase("ABC"));//대소문자 구분 안함
+		ch = '1';
+		System.out.printf("ch=%c, '0' <= ch && ch <= '9' = %b\n", ch, '0' <= ch && ch <= '9');
+		
+		ch = 'a';
+		System.out.printf("ch=%c, 'a' <= ch && ch <= 'z' = %b\n", ch, 'a' <= ch && ch <= 'z');
+		
+		ch = 'A';
+		System.out.printf("ch=%c, 'A' <= ch && ch <= 'Z' = %b\n", ch, 'A' <= ch && ch <= 'Z');
+		
+		ch = 'q';
+		System.out.printf("ch=%c, ch == 'q' || ch == 'Q' = %b\n", ch, ch == 'q' || ch == 'Q');
 	}
 }
