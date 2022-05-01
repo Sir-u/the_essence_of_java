7b55480e34a878922ddae395fc990911ab7e676f
Siru <ehdgur9553@naver.com>
2022-05-01 09:24:49 +0900

    5/1 commit

--- src/ch2/ScannerEx.java
+++ src/ch2/ScannerEx.java
@@ -6,7 +6,7 @@ public class ScannerEx {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.printf"두자리 정수를 하나 입력해주세요.>");
+		System.out.printf("두자리 정수를 하나 입력해주세요.>");
 		String input = scanner.nextLine();
 		int num = Integer.parseInt(input);
 		
